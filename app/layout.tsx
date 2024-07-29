05e7e6e2223f513071fb3d42de0c1cd6815430b0
Brian Tang <ashrielbrian@gmail.com>
2024-07-30 00:35:32 +0800

    Adds mdx and main blog page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex justify-center items-center bg-white p-24">
+          <div className="h-screen prose">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
