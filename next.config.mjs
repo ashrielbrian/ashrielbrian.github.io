73c9e2cb1cac2bec19d62c9ee42d13293c0b1837
Brian Tang <ashrielbrian@gmail.com>
2024-07-31 00:16:26 +0800

    Removes basepath since using ashrielbrian.com

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,6 @@ const nextConfig = {
   output: "export",
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // set the base path as by default, nextjs maps all static assets to /,
-  // but Github Pages uses the project name, in my case, /ashrielbrian
-  basePath: "/ashrielbrian",
 };
 
 export default withMDX(nextConfig);
