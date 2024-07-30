f8bc23e22fb4684c8307fce15bbf2fa9a9e66f0a
Brian Tang <ashrielbrian@gmail.com>
2024-07-30 23:38:11 +0800

    Comments out options

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withMDX = nextMDX();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // see: https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages
   output: "export",
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
