9a59697fbc5d520f0d1f4ab8391bd56058687664
Brian Tang <ashrielbrian@gmail.com>
2024-07-30 23:36:46 +0800

    Adds basepath; output set to export

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,12 @@ const withMDX = nextMDX();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // set the base path as by default, nextjs maps all static assets to /,
+  // but Github Pages uses the project name, in my case, /ashrielbrian
+  basePath: "/ashrielbrian",
 };
 
 export default withMDX(nextConfig);
